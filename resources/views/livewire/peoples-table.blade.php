b2fc431c6c32ff8320a33a1bff346f4f3429bd3d
Tiago Calado <1tiagocalado8@gmail.com>
2023-03-29 14:21:14 +0100

    Fixing datatables search placeholders

--- resources/views/livewire/peoples-table.blade.php
+++ resources/views/livewire/peoples-table.blade.php
@@ -1,7 +1,7 @@
 <div>
     <div class="w-full flex pb-10 space-x-8 justify-center">
         <div class="w-3/6 mx-1">
-            <input wire:model="search" wire:model.debounce.300ms="search" type="search" class="appearance-none block w-full bg-gray-200 text-gray-700 border border-gray-200 rounded py-3 px-4 leading-tight focus:outline-none focus:bg-white focus:border-gray-500" placeholder="Search starships...">
+            <input wire:model="search" wire:model.debounce.300ms="search" type="search" class="appearance-none block w-full bg-gray-200 text-gray-700 border border-gray-200 rounded py-3 px-4 leading-tight focus:outline-none focus:bg-white focus:border-gray-500" placeholder="Search characters...">
         </div>
         <div>
             <select wire:model="orderBy" class="block appearance-none w-full bg-gray-200 border border-gray-200 text-gray-700 py-3 px-4 pr-8 rounded leading-tight focus:outline-none focus:bg-white focus:border-gray-500">
