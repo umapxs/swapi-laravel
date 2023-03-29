1db15496fb3dbdf4e24309fec455d7ce75f602f9
Tiago Calado <1tiagocalado8@gmail.com>
2023-03-29 13:02:03 +0100

    Change the overall look of the Datatables

--- resources/views/tables/starship-table.blade.php
+++ resources/views/tables/starship-table.blade.php
@@ -6,8 +6,11 @@
     </x-slot>
 
     <div class="py-12">
+        <div class="max-w-7xl mx-auto sm:px-6 lg:px-8">
+            <div class="bg-white dark:bg-gray-800 overflow-hidden shadow-sm sm:rounded-lg"
+            style="margin-top: 3rem;">
                 <div class="p-6 text-gray-900 dark:text-gray-100">
-                        <livewire:starships-table>
+                    <livewire:starships-table>
                 </div>
             </div>
         </div>
