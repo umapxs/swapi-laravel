7c4ee12d8b5e6ac0d85d2f347bb20873f9cfb3f5
Tiago Calado <1tiagocalado8@gmail.com>
2023-04-03 16:53:12 +0100

    Make small hover changes

--- resources/views/layouts/guest.blade.php
+++ resources/views/layouts/guest.blade.php
@@ -5,6 +5,7 @@
         <meta name="viewport" content="width=device-width, initial-scale=1">
         <meta name="csrf-token" content="{{ csrf_token() }}">
         <link rel="icon" type="image/x-icon" href="{{ config('app.favicon', '/images/star-wars2.png') }}">
+        <script src="https://cdn.tailwindcss.com"></script>
 
         <title>{{ config('app.name', 'swapiProject') }}</title>
 
@@ -16,15 +17,17 @@
         @vite(['resources/css/app.css', 'resources/js/app.js'])
     </head>
     <body class="font-sans text-gray-900 antialiased">
-        <div class="min-h-screen flex flex-col sm:justify-center items-center pt-6 sm:pt-0 bg-gray-100">
+        <div class="min-h-screen flex flex-col sm:justify-center items-center pt-6 sm:pt-0 bg-white">
             <div>
                 <a href="/">
                     <x-application-logo class="w-20 h-20 fill-current text-gray-500" />
                 </a>
             </div>
 
-            <div class="w-full sm:max-w-md mt-6 px-6 py-4 bg-white shadow-md overflow-hidden sm:rounded-lg">
-                {{ $slot }}
+            <div class="w-full sm:max-w-md mt-6 px-6 py-4 bg-white sm:rounded-lg outline outline-1 outline-offset-4 outline-gray-200 mx-4">
+                <div class="px-3 py-1">
+                    {{ $slot }}
+                </div>
             </div>
         </div>
     </body>
