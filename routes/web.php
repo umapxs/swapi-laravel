cb6ddda7798ce8003a1c2655fbc0c5f418e6c24c
Tiago Calado <1tiagocalado8@gmail.com>
2023-04-17 09:23:28 +0100

    [2fa] Fix access to other URIs without 2fa

--- routes/web.php
+++ routes/web.php
@@ -121,7 +121,6 @@ Route::post('films/{films:id}/comments', [FilmsCommentsController::class, 'store
  * Export Routes
  *
  **/
-
 // Excel
 Route::post('starships/export', [StarshipsController::class, 'export'])->name('starships.export');
 Route::post('peoples/export', [PeoplesController::class, 'export'])->name('peoples.export');
@@ -160,6 +159,64 @@ Route::middleware(['2fa'])->group(function () {
 
         return view('dashboard')->with($data);
     })->middleware(['auth', 'verified'])->name('dashboard');
+
+    // Profile related
+    Route::get('/profile', [ProfileController::class, 'edit'])->name('profile.edit');
+    Route::patch('/profile', [ProfileController::class, 'update'])->name('profile.update');
+    Route::delete('/profile', [ProfileController::class, 'destroy'])->name('profile.destroy');
+    // Route::delete('/2fa', [ProfileController::class, 'destroy2fa'])->name('profile.destroy.2fa');
+
+    // Starship related
+    Route::get('/starships/create', [StarshipsController::class, 'create'])->name('starships.create');
+    Route::get('/starships', [StarshipsController::class, 'default'])->name('starships.default');
+    Route::get('/starships/store', [StarshipsController::class, 'store'])->name('starships.store');
+    Route::get('/table/starship',[StarshipsController::class, 'index'])->name('starships.index');
+    Route::get('/starships/{starship:id}',[StarshipsController::class, 'show'])->name('starships.show');
+    Route::delete('/starships/{id}', [StarshipsController::class, 'destroy'])->name('starships.destroy');
+    Route::post('/starships/storeCreate', [StarshipsController::class, 'storeCreate'])->name('starships.storeCreate');
+    Route::get('/starships/edit/{people:id}', [StarshipsController::class, 'edit'])->name('starships.edit');
+    Route::put('/starships/{id}', [StarshipsController::class, 'update'])->name('starships.update');
+
+    // People related
+    Route::get('/peoples/create', [PeoplesController::class, 'create'])->name('peoples.create');
+    Route::get('/peoples', [PeoplesController::class, 'default'])->name('peoples.default');
+    Route::get('/peoples/store', [PeoplesController::class, 'store'])->name('peoples.store');
+    Route::get('/table/people',[PeoplesController::class, 'index'])->name('peoples.index');
+    Route::get('/peoples/{people:id}',[PeoplesController::class, 'show'])->name('peoples.show');
+    Route::delete('/peoples/{id}', [PeoplesController::class, 'destroy'])->name('peoples.destroy');
+    Route::post('/peoples/storeCreate', [PeoplesController::class, 'storeCreate'])->name('peoples.storeCreate');
+    Route::get('/peoples/edit/{people:id}', [PeoplesController::class, 'edit'])->name('peoples.edit');
+    Route::put('/peoples/{id}', [PeoplesController::class, 'update'])->name('peoples.update');
+
+    // Film  related
+    Route::get('/films/create', [FilmsController::class, 'create'])->name('films.create');
+    Route::get('/films', [FilmsController::class, 'default'])->name('films.default');
+    Route::get('/films/store', [FilmsController::class, 'store'])->name('films.store');
+    Route::get('/table/film',[FilmsController::class, 'index'])->name('films.index');
+    Route::get('/films/{film:id}',[FilmsController::class, 'show'])->name('films.show');
+    Route::delete('/films/{id}', [FilmsController::class, 'destroy'])->name('films.destroy');
+    Route::patch('/films/storeCreate', [FilmsController::class, 'storeCreate'])->name('films.storeCreate');
+    Route::post('/films/storeCreate', [FilmsController::class, 'storeCreate'])->name('films.storeCreate');
+    Route::get('/films/edit/{film:id}', [FilmsController::class, 'edit'])->name('films.edit');
+    Route::put('/films/{id}', [FilmsController::class, 'update'])->name('films.update');
+
+    /**
+     * Comments
+     *
+     **/
+    Route::post('starships/{starships:id}/comments', [StarshipsCommentsController::class, 'store'])->name('starshipsComments.store');
+    Route::post('peoples/{peoples:id}/comments', [PeoplesCommentsController::class, 'store'])->name('peoplesComments.store');
+    Route::post('films/{films:id}/comments', [FilmsCommentsController::class, 'store'])->name('filmsComments.store');
+
+    // Excel
+    Route::post('starships/export', [StarshipsController::class, 'export'])->name('starships.export');
+    Route::post('peoples/export', [PeoplesController::class, 'export'])->name('peoples.export');
+    Route::post('films/export', [FilmsController::class, 'export'])->name('films.export');
+
+    // PDF
+    Route::get('/starships/{id}/exportPDF', [StarshipsController::class, 'exportPDF'])->name('starships.exportPDF');
+    Route::get('/films/{id}/exportPDF', [FilmsController::class, 'exportPDF'])->name('films.exportPDF');
+    Route::get('/peoples/{id}/exportPDF', [PeoplesController::class, 'exportPDF'])->name('peoples.exportPDF');
 
     Route::post('/2fa', function () {
         return redirect('dashboard');
