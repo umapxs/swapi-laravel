1452ad89acef678d27cc70c96498dae32dc46975
Tiago Calado <1tiagocalado8@gmail.com>
2023-04-27 13:02:51 +0100

    [PWA] Change PWA icons

--- config/laravelpwa.php
+++ config/laravelpwa.php
@@ -13,49 +13,49 @@ return [
         'status_bar'=> 'black',
         'icons' => [
             '72x72' => [
-                'path' => '/images/icons/icon-72x72.png',
+                'path' => '/images/star-wars2.png',
                 'purpose' => 'any'
             ],
             '96x96' => [
-                'path' => '/images/icons/icon-96x96.png',
+                'path' => '/images/star-wars2.png',
                 'purpose' => 'any'
             ],
             '128x128' => [
-                'path' => '/images/icons/icon-128x128.png',
+                'path' => '/images/star-wars2.png',
                 'purpose' => 'any'
             ],
             '144x144' => [
-                'path' => '/images/icons/icon-144x144.png',
+                'path' => '/images/star-wars2.png',
                 'purpose' => 'any'
             ],
             '152x152' => [
-                'path' => '/images/icons/icon-152x152.png',
+                'path' => '/images/star-wars2.png',
                 'purpose' => 'any'
             ],
             '192x192' => [
-                'path' => '/images/icons/icon-192x192.png',
+                'path' => '/images/star-wars2.png',
                 'purpose' => 'any'
             ],
             '384x384' => [
-                'path' => '/images/icons/icon-384x384.png',
+                'path' => '/images/star-wars2.png',
                 'purpose' => 'any'
             ],
             '512x512' => [
-                'path' => '/images/icons/icon-512x512.png',
+                'path' => '/images/star-wars2.png',
                 'purpose' => 'any'
             ],
         ],
         'splash' => [
-            '640x1136' => '/images/icons/splash-640x1136.png',
-            '750x1334' => '/images/icons/splash-750x1334.png',
-            '828x1792' => '/images/icons/splash-828x1792.png',
-            '1125x2436' => '/images/icons/splash-1125x2436.png',
-            '1242x2208' => '/images/icons/splash-1242x2208.png',
-            '1242x2688' => '/images/icons/splash-1242x2688.png',
-            '1536x2048' => '/images/icons/splash-1536x2048.png',
-            '1668x2224' => '/images/icons/splash-1668x2224.png',
-            '1668x2388' => '/images/icons/splash-1668x2388.png',
-            '2048x2732' => '/images/icons/splash-2048x2732.png',
+            '640x1136' => '/images/star-wars2.png',
+            '750x1334' => '/images/star-wars2.png',
+            '828x1792' => '/images/star-wars2.png',
+            '1125x2436' => '/images/star-wars2.png',
+            '1242x2208' => '/images/star-wars2.png',
+            '1242x2688' => '/images/star-wars2.png',
+            '1536x2048' => '/images/star-wars2.png',
+            '1668x2224' => '/images/star-wars2.png',
+            '1668x2388' => '/images/star-wars2.png',
+            '2048x2732' => '/images/star-wars2.png',
         ],
         'shortcuts' => [
             [
@@ -63,7 +63,7 @@ return [
                 'description' => 'Shortcut Link 1 Description',
                 'url' => '/shortcutlink1',
                 'icons' => [
-                    "src" => "/images/icons/icon-72x72.png",
+                    "src" => "/images/star-wars2.png",
                     "purpose" => "any"
                 ]
             ],
